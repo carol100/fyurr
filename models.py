c72ce4d45ac4f575cc529cbb1464ff078d85ae44
caroline <caroline@turelabs.com>
2022-08-11 23:42:59 +0300

    database Models file

---
+++ models.py
@@ -0,0 +1,34 @@
+from email.policy import default
+from app import db
+
+
+
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+  
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean(), default=True)
+    seeking_description = db.Column(db.Boolean, nullable=False, default=False)
+    shows = db.relationship('Venue', secondary='shows', backref='Artist', lazy=True)
+
+
+
+class Show(db.Model):
+    __tablename__ = 'show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime())
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+    venue_id = db.Column(db.Integer(), db.Foreign('Venue.id'), nullable=True)
+
+
